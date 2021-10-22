4cbf31da6f361b2fc1ab1bede4ffae4abe7572ee
Laura Hofer <ljhofer@gmail.com>
2021-10-22 08:07:19 -0500

    Added intial file structure, installed required packages,  and created intial question array

---
+++ server.js
@@ -0,0 +1,40 @@
+// Require in what we need
+
+const ListPrompt = require("inquirer/lib/prompts/list");
+
+
+
+
+// Inital question
+const initialQuestion = [
+    {
+        type: "list",
+        message: "What would you like to do?",
+        name: "initQuestion",
+        choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department" ]
+    }
+]
+
+
+// Question sets
+
+
+
+// Use index.js for routing
+
+
+
+
+
+
+
+
+
+
+
+// Module exports
+
+
+
+
+
