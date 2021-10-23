4d02da7dfccc9becc6183009a196f304a65c3a15
Laura Hofer <ljhofer@gmail.com>
2021-10-23 11:34:59 -0500

    Added functions for showing tables of all employees, roles, and departments when prompted

--- server.js
+++ server.js
@@ -26,76 +26,103 @@ const initialQuestion = [
     }
 ]
 
+const addEmployeeQuestions = [
 
+
+]
+
+const addRoleQuestions = [
+
+
+]
+
+const addDepartmentQuestions = [
+
+    
+]
 // Starts app by calling intial question
 start = () => {
     inquirer
         .prompt(initialQuestion)
 
         .then(response => {
+
             // Routes user to next function based on answer to intial question
-            if (response === "View All Employees"){
-                viewAllEmployees();
+            if (response.initQuestion === "View All Employees"){
+                viewAllEmployees();  
             } 
-            else if (response === "Add Employee"){
+            else if (response.initQuestion === "Add Employee"){
                 addEmployee();
             } 
-            else if (response === "Update Employee Role"){
+            else if (response.initQuestion === "Update Employee Role"){
                 updateEmployeeRole();
             } 
-            else if (response === "View All Roles"){
+            else if (response.initQuestion === "View All Roles"){
                 viewAllRoles();
             }
-            else if (response === "Add Role"){
+            else if (response.initQuestion === "Add Role"){
                 addRole();
             }
-            else if (response === "View All Departments"){
+            else if (response.initQuestion === "View All Departments"){
                 viewAllDepartments();
             }
-            else if (response === "Add Department"){
+            else if (response.initQuestion === "Add Department"){
                 addDepartment();
             }
-            else return;        
+            else return; 
         })
-
 }
 
 // Shows all employees in the database
 viewAllEmployees = () => {
-
-}
+    db.query(`SELECT * FROM employee`, (err, res) => {
+        if (err) return res.status(400).console.log(err)
+        console.table(res);
+        start();
+    })
+};
 
  // Adds an employee to the database
 addEmployee = () => {
 
-}
+};
 
 updateEmployeeRole = () => {
 
-}
+};
 
 // Shows all roles in the database
 viewAllRoles = () => {
-
-}
+    db.query(`SELECT * FROM role`, (err, res) => {
+        if (err) return res.status(400).console.log(err)
+        console.table(res);
+        start();
+    })
+};
 
 // Adds a role to the database
 addRole = () => {
 
-}
+};
 
 // Shows all departments in the database
 viewAllDepartments = () => {
-
-}
-
+    db.query(`SELECT * FROM department`, (err, res) => {
+        if (err) return res.status(400).console.log(err)
+        console.table(res);
+        start();
+    })
+};
 
 // Adds adepartment to the database
 addDepartment = () => {
 
-}
+};
 
 
+
+// Calls start function 
+start();
 
 
 
