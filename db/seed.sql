be5ab4cee55a6359bf032bc1498411318091158f
Laura Hofer <ljhofer@gmail.com>
2021-10-23 09:13:23 -0500

    Added an employee of each type to seed file

--- db/seed.sql
+++ db/seed.sql
@@ -1,17 +1,26 @@
 INSERT INTO department (name)
-VALUES ("Front Office"),
-        ("Barn"),
-        ("Nursery");
+    VALUES ("Front Office"),
+           ("Barn"),
+           ("Nursery");
 
-INSERT INTO roles (title, salary, department_id)
-    VALUES ("Manager", 75000, ),
-           ("Employee", 55000, ),
-           ("Volunteer", 0, );
+INSERT INTO role (title, salary, department_id)
+    VALUES ("Office Manager", 99000, 1),
+           ("Barn Manager", 99000, 2),
+           ("Nursery Manager", 99000, 3),
+           ("Office Employee", 75000, 1),
+           ("Barn Employee", 75000, 2),
+           ("Nursery Employee", 75000, 3),
+           ("Office Intern", 30000, 1),
+           ("Barn Intern", 30000, 2),
+           ("Nursery Intern", 30000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-    VARCHAR ("Laura", "Hofer", ),
-            ("Jessica", "Davies-Lopez", ),
-            ("Mark", "Hiner", ),
-            ("Bridget", "Finnegan Forbes", ),
-            ("Jason", "Kemp", ),
-            ("Julie", "Knopp", )
+    VALUES ("Julie", "Knopp", 1, null),
+           ("Laura", "Hofer", 2, null),
+           ("Mark", "Hiner", 3, null),
+           ("Jessica", "Davies-Lopez", 4, 1),
+           ("Bridget", "Finnegan Forbes", 5, 2),
+           ("Kurt", "Pederson", 6, 3),
+           ("Minnie", "Hofer", 7, 1),
+           ("Huxley", "Hofer", 8, 2),
+           ("Marian", "Maciej-Hiner", 9, 3);
